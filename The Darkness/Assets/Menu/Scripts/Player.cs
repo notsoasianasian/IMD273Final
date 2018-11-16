49efd05642cc729f4c11daf6cb17093173b90039
notsoasianasian <44407935+notsoasianasian@users.noreply.github.com>
2018-11-16 00:12:06 -0500

    Assets

---
+++ The Darkness/Assets/Menu/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+	//variables
+
+	public float maxHealth, maxThirst, maxHunger;
+	public float thirstIncreaseRate, hungerIncreaseRate;
+	private float health, thirst, hunger;
+
+	public bool dead;
+
+	//functions
+	public void Start()
+
+	{
+		health = maxHealth;
+	}
+
+	public void Update()
+
+	{
+		// thirst and hunger increase
+		if (!dead) 
+		{
+			thirst += thirstIncreaseRate * Time.deltaTime;
+			hunger += hungerIncreaseRate * Time.deltaTime;
+		}
+
+		if (thirst >= maxThirst)
+			Die();
+
+		if (hunger >= maxHunger)
+			Die ();
+
+	}
+
+	public void Die()
+	{
+		dead = true;
+		print ("you are dead");
+	}
+
+	public void Drink(float decreaseRate)
+
+	{
+		thirst -= decreaseRate;
+	}
+
+	public  void Eat(float decreaseRate)
+
+	{
+		
+	}
+
+}
