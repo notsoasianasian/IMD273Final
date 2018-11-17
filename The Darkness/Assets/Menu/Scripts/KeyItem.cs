e60eb41c0b18c875f6c2668fcd6098c5adc1c4d1
notsoasianasian <44407935+notsoasianasian@users.noreply.github.com>
2018-11-16 23:12:38 -0500

    new

---
+++ The Darkness/Assets/Menu/Scripts/KeyItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItem : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+
+	{
+		if (collider.gameObject.tag == "player") 
+		{
+			
+		}
+	}
+
+	}
+
---
+++ The Darkness/Assets/Menu/Scripts/Swimmer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swimmer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		RenderSettings.fog = false;
+		RenderSettings.fogColor = new Color (0.2f, 0.4f, 0.8f, 0.5f);
+		RenderSettings.fogDensity = 0.04f;
+		
+	}
+
+	bool isUnderWater()
+	{
+		return gameObject.transform.position.y < 48;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RenderSettings.fog = isUnderWater ();
+
+		
+	}
+}
